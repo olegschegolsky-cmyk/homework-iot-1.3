5360069f1986f55954134e1cd80ecdd28f57b14d
Oleg11111112 <o.schegolsky08@gmail.com>
2025-12-12 09:47:08 +0200

    fixed csv logic and logging decorator

--- lab6.py
+++ lab6.py
@@ -1,6 +1,7 @@
 import csv
 import os
 import logging
+import functools
 
 class FileCorrupted(Exception):
     pass
@@ -10,40 +11,39 @@ class FileNotFound(Exception):
 
 def logged(exc_cls, mode):
     def decorator(func):
-        def wrapers(*args, **kwargs):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
             logger = logging.getLogger("LabLogger")
             logger.setLevel(logging.ERROR)
-            
+
+            if logger.hasHandlers():
+                logger.handlers.clear()
 
             if mode == "file":
                 handler = logging.FileHandler("log.txt", mode='a', encoding='utf-8')
             else:
                 handler = logging.StreamHandler()
 
-            if logger.hasHandlers():
-                logger.handlers.clear()
-
-            
             format = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
             handler.setFormatter(format)
             logger.addHandler(handler)
-            
+
             try:
                 return func(*args, **kwargs)
             except exc_cls as e:
                 logger.error(f"помилка в {func.__name__}: {e}")
                 raise e
-        return wrapers
+        return wrapper
     return decorator
 
 class Csv:
     def __init__(self, path):
         self.path = path
-        if not os.path.exists(self.path):
-            raise FileNotFound(f"файл не знайдено: {self.path}")
 
     @logged(FileCorrupted, "console")
     def read(self):
+        if not os.path.exists(self.path):
+            raise FileNotFound(f"файл не знайдено: {self.path}")
         try:
             with open(self.path, 'r', newline='', encoding='utf-8') as f:
                 return list(csv.reader(f))
@@ -66,11 +66,10 @@ class Csv:
         except OSError as e:
             raise FileCorrupted(f"помилка дописування: {e}")
 
-
 try:
     manager = Csv("data.csv")
     manager.write([["Name", "Group"]])
-    manager.write([["Oleg", "IR-11"]])
+    manager.append([["Oleg", "IR-11"]])
     manager.append([["Hanna", "IR-12"]])
     manager.append([["Bodia", "IR-12"]])
     print(manager.read())
